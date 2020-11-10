799cb496bf6adccec6ec9927b2dceaff5726056c
Teguh <teguh.wck95@gmail.com>
2020-11-10 17:37:51 +0700

    Creating file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>An Online Course Website | Linguo</title>
+</head>
+<body>
+    <h1>Main Page</h1>
+    <p>This page that firstly shows when user try to visit the website before login. </p>
+</body>
+</html>
