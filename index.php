1f619e07fdf8d5bc31c36d25234d968e348661a6
twicks95 <teguh.wck95@gmail.com>
2020-11-11 19:00:01 +0700

    Adding triggers to show a login panel using javascript

--- index.php
+++ index.php
@@ -33,19 +33,20 @@ require "functions.php";
         </div>
     </nav>
     <div class="content-wrapper">
-        <h1 class="title">Learn English, French And Germany</h1>
+        <h1 class="title">Learn English, French, And Germany</h1>
         <p class="description">Linguo provides three most used foreign languages to enhance your language skills beyond your current level</p>
 
         <!-- <hr> -->
 
         <div class="btn-login-group">
-            <p>You are not logged in</p><a href="pages/login.php"><i class="icon" data-feather="log-in"></i>Login</a>
+            <p>You are not logged in</p><a id="btn-login"><i class="icon" data-feather="log-in"></i>Login</a>
         </div>
     </div>
 
     <div id="login-panel">
         <div class="login-wrapper">
             <div class="login-background">
+                <i class="btn-x" data-feather="x"></i>
                 <h3>User Login</h3>
                 <form class="login-form" action="" method="POST">
                     <div class="form-group">
@@ -69,6 +70,19 @@ require "functions.php";
     <script src="https://unpkg.com/feather-icons"></script>
     <script>
         feather.replace()
+    </script>
+    <script>
+        const btnLogin = document.getElementById("btn-login");
+        const btnX = document.getElementsByClassName("btn-x");
+        const loginPanel = document.getElementById("login-panel");
+
+        btnLogin.addEventListener("click", () => {
+            loginPanel.style.display = "block";
+        })
+
+        btnX[0].addEventListener("click", () => {
+            loginPanel.style.display = "none";
+        })
     </script>
 </body>
 
